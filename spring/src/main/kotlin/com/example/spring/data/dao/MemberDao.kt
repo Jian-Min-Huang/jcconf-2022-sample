bfcf90809ef5e09b8aee00ffee580a853ab9187f
Vincent Huang MacBook <yfr.huang@hotmail.com>
2022-10-06 21:52:08 +0800

    :tada: refactor: init commit

--- spring/src/main/kotlin/com/example/spring/data/dao/MemberDao.kt
+++ spring/src/main/kotlin/com/example/spring/data/dao/MemberDao.kt
@@ -2,6 +2,8 @@ package com.example.spring.data.dao
 
 import com.example.spring.data.po.MemberPo
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Component
 
+@Component
 interface MemberDao : CrudRepository<MemberPo, Long> {
 }
